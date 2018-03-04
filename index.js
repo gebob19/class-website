293cd15f19735f0b89def75327ff0a5f4423a231
Brennan Gebotys <gebob19@gmail.com>
2018-03-04 18:17:49 -0500

    client folder is now publicly avaliable

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
 const server = require('http').Server(app);
+const path = require('path');
+
+app.use(express.static('client'));
 
 app.get('/', function(req, res) {
-    res.sendFile(__dirname+'/index.html');
+    res.sendFile(index.html);
 });
 
 server.listen(3000);
