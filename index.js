2b387abefc6bed4cbd220dba196d25f2dd15c799
Omar Chehab <omarchehab98@gmail.com>
2018-03-04 22:56:54 -0500

    Setup file directory according to required structure

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const app = express();
 const server = require('http').Server(app);
 const path = require('path');
 
-app.use(express.static('client'));
+app.use(express.static('Assignment2/src'));
 
 server.listen(3000);
