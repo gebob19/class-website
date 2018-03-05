2e8b9d8187b1989bf25258c5f19a70b0dd559dc5
Omar Chehab <omarchehab98@gmail.com>
2018-03-05 10:09:32 -0500

    Change from express to browsersync

--- index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express();
-const server = require('http').Server(app);
-const path = require('path');
-
-app.use(express.static('Assignment2/src'));
-
-server.listen(3000);
