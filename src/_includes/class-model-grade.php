fb96da98e8055f7df1bce98b7c3a67de52e31db8
Omar Chehab <omarchehab98@gmail.com>
2018-03-29 17:24:20 -0400

    Update grade model and add tests to load

--- src/_includes/class-model-grade.php
+++ src/_includes/class-model-grade.php
@@ -3,16 +3,99 @@ class Grade implements Model {
   public static function create_table() {
     query("CREATE TABLE IF NOT EXISTS `Grade` (
       `student_username` varchar(255) NOT NULL,
-      `gradable_name` varchar(255) NOT NULL,
-      `mark` numeric NOT NULL,
-      `remark` boolean DEFAULT false,
+      `gradable_id` varchar(255) NOT NULL,
+      `grade` numeric NOT NULL,
+      `remark_status` ENUM('not-requested', 'requested', 'approved', 'remarked', 'declined') DEFAULT 'not-requested',
       `remark_message` text NULL,
-      `remark_status` ENUM('pending', 'approved', 'remarked', 'declined') NULL,
       `created_at` datetime NOT NULL,
       `updated_at` datetime NOT NULL,
-      primary key (`student_username`, `gradable_name`),
+      primary key (`student_username`, `gradable_id`),
       foreign key (`student_username`) references `User` (`username`),
-      foreign key (`gradable_name`) references `Gradable` (`id`)
+      foreign key (`gradable_id`) references `Gradable` (`id`)
     )");
+  }
+
+  public static function selectByStudentUsername($student_username) {
+    return query("SELECT `student_username`,
+      `gradable_id`,
+      `grade`,
+      `remark_status`,
+      `remark_message`,
+      `created_at`,
+      `updated_at`
+      FROM `Grade`
+      WHERE `student_username` = ?", [
+      's',
+      $student_username
+    ]);
+  }
+  
+  public static function selectByGradableId($gradable_id) {
+    return query("SELECT `student_username`,
+      `gradable_id`,
+      `grade`,
+      `remark_status`,
+      `remark_message`,
+      `created_at`,
+      `updated_at`
+      FROM `Grade`
+      WHERE `gradable_id` = ?", [
+      's',
+      $gradable_id
+    ]);
+  }
+
+  public static function insert($data) {
+    $now = now();
+    return query("INSERT INTO `Grade` (
+      `student_username`,
+      `gradable_id`,
+      `grade`,
+      `remark_status`,
+      `remark_message`,
+      `created_at`,
+      `updated_at`
+    ) VALUES (
+      ?,
+      ?,
+      ?,
+      ?,
+      ?,
+      ?,
+      ?
+    )", [
+      'ssdssss',
+      $data->student_username,
+      $data->gradable_id,
+      $data->grade,
+      $data->remark_status,
+      $data->remark_message,
+      $now,
+      $now
+    ]);
+  }
+  
+  public static function updateById($id, $types, $set) {
+    $now = now();
+    
+    $stmt_set = [];
+    $stmt_values = [];
+    foreach ($set as $column => $value) {
+      $stmt_set[] = "`$column` = ?";
+      $stmt_values[] = $value;
+    }
+
+    $types .= 's';
+    $stmt_set[] = "`updated_at` = ?";
+    $stmt_values[] = $now;
+
+    $stmt_set = implode(', ', $stmt_set);
+    $types .= 's';
+    
+    return query("UPDATE `Gradable` SET $stmt_set WHERE `id` = ?", array_merge(
+      [$types],
+      $stmt_values,
+      [$id]
+    ));
   }
 }
--- src/load.php
+++ src/load.php
@@ -1,6 +1,11 @@
 <?php
-function debug($data) {
-  highlight_string("<? ".var_export($data, true)."\n");
+function debug() {
+  $args = func_get_args();
+  foreach ($args as $arg) {
+    highlight_string("<? " . var_export($arg, true) . " ?>");
+    echo '<br>';
+  }
+  echo '<br>';
 }
 
 require_once ABSPATH . INCLUDES . 'global-db.php';
@@ -11,6 +16,8 @@ require_once ABSPATH . INCLUDES . 'interface-model.php';
 require_once ABSPATH . INCLUDES . 'class-model-user.php';
 require_once ABSPATH . INCLUDES . 'class-model-gradable.php';
 require_once ABSPATH . INCLUDES . 'class-model-grade.php';
+require_once ABSPATH . INCLUDES . 'class-model-feedback.php';
+require_once ABSPATH . INCLUDES . 'class-model-announcement.php';
 require_once ABSPATH . INCLUDES . 'component-meta.php';
 require_once ABSPATH . INCLUDES . 'component-header.php';
 require_once ABSPATH . INCLUDES . 'component-footer.php';
@@ -21,54 +28,67 @@ require_once ABSPATH . INCLUDES . 'layout-collection-list.php';
 require_once ABSPATH . INCLUDES . 'layout-collection-single.php';
 require_once ABSPATH . INCLUDES . 'layout-announcements.php';
 
-User::create_table();
-Gradable::create_table();
-Grade::create_table();
-Feedback::create_table();
-Announcement::create_table();
-
-User::insert((object) [
-  'username' => 'chehabom',
-  'password' => 'chehabom',
-  'role' => 'student'
-]);
-User::insert((object) [
+debug('User::create_table', User::create_table());
+debug('Gradable::create_table', Gradable::create_table());
+debug('Grade::create_table', Grade::create_table());
+debug('Feedback::create_table', Feedback::create_table());
+debug('Announcement::create_table', Announcement::create_table());
+try {
+  debug('User::insert', User::insert((object) [
+    'username' => 'chehabom',
+    'password' => 'chehabom',
+    'role' => 'student'
+  ]));
+} catch (DatabaseException $err) {
+  debug('User::insert', $err->getMessage());
+}
+debug('User::insert', User::insert((object) [
   'username' => 'gebotysb',
   'password' => 'gebotysb',
   'role' => 'instructor'
-]);
-User::updateByUsername('chehabom', 's', [
+]));
+debug('User::updateByUsername', User::updateByUsername('chehabom', 's', [
   'role' => 'ta',
-]);
-debug(User::selectByUsername('gebotysb'));
-debug(User::selectAllByRole('ta'));
-User::deleteByUsername('gebotysb');
-
-Gradable::insert((object) [
-  'id' => 'a1',
-  'weight' => 0.25,
-]);
-Gradable::insert((object) [
-  'id' => 'a2',
-  'weight' => 0.25,
-]);
-Gradable::updateById('a2', 'd', (object) [
+]));
+debug('User::selectByUsername', User::selectByUsername('gebotysb'));
+debug('User::selectAllByRole', User::selectAllByRole('ta'));
+debug('User::deleteByUsername', User::deleteByUsername('gebotysb'));
+try {
+  debug('Gradable::insert', Gradable::insert((object) [
+    'id' => 'a1',
+    'weight' => 0.25,
+  ]));
+} catch (DatabaseException $err) {
+  debug('Gradable::insert', $err->getMessage());
+}
+try {
+  debug('Gradable::insert', Gradable::insert((object) [
+    'id' => 'a2',
+    'weight' => 0.25,
+  ]));
+} catch (DatabaseException $err) {
+  debug('Gradable::insert', $err->getMessage());
+}
+debug('Gradable::updateById', Gradable::updateById('a2', 'd', (object) [
   'weight' => 0.20
-]);
-debug(Gradable::selectById('a1'));
-debug(Gradable::selectAll());
-
-Feedback::insert((object) [
+]));
+debug('Gradable::selectById', Gradable::selectById('a1'));
+debug('Gradable::selectAll', Gradable::selectAll());
+debug('Grade::insert', Grade::insert((object) [
+  'student_username' => 'chehabom',
+  'gradable_id' => 'a1',
+  'grade' => 0.85,
+  'remark_status' => 'not-requested',
+  'remark_message' => NULL,
+]));
+debug('Feedback::insert', Feedback::insert((object) [
   'message'=>'abbas steals from w3schools'
-]);
-
-debug(Feedback::selectAll());
-
-Announcement::insert((object) [
+]));
+debug('Feedback::selectAll', Feedback::selectAll());
+debug('Announcement::insert', Announcement::insert((object) [
   'title' => 'New Instructor',
   'content' => 'Brennan Gebotys will be your new prof, abbas has stepped down',
   'author_username' => 'chehabom',
-]);
-
-debug(Announcement::selectAll());
-debug(Announcement::selectById(1));
+]));
+debug('Announcement::selectAll', Announcement::selectAll());
+debug('Announcement::selectById', Announcement::selectById(1));
