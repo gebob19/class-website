4e26863aa6e51a7db88980e0fd1771a8ad805a46
Omar Chehab <omarchehab98@gmail.com>
2018-03-28 23:25:29 -0400

    Add Grade Model

---
+++ src/_includes/class-model-grade.php
@@ -0,0 +1,18 @@
+<?php
+class Grade implements Model {
+  public static function create_table() {
+    query("CREATE TABLE IF NOT EXISTS Grade (
+      `student_username` varchar(255) NOT NULL,
+      `gradable_name` varchar(255) NOT NULL,
+      `mark` numeric NOT NULL,
+      `remark` boolean DEFAULT false,
+      `remark_message` text NULL,
+      `remark_status` ENUM('pending', 'approved', 'remarked', 'declined') NULL,
+      `created_at` datetime NOT NULL,
+      `updated_at` datetime NOT NULL,
+      constraint PK_Grade primary key (student_username, gradable_name),
+      foreign key (student_username) references `User` (username),
+      foreign key (gradable) references `Gradeable` (id)
+    )");
+  }
+}
--- src/load.php
+++ src/load.php
@@ -9,6 +9,7 @@ require_once ABSPATH . INCLUDES . 'function-query.php';
 require_once ABSPATH . INCLUDES . 'interface-model.php';
 require_once ABSPATH . INCLUDES . 'class-model-user.php';
 require_once ABSPATH . INCLUDES . 'class-model-gradable.php';
+require_once ABSPATH . INCLUDES . 'class-model-grade.php';
 require_once ABSPATH . INCLUDES . 'component-meta.php';
 require_once ABSPATH . INCLUDES . 'component-header.php';
 require_once ABSPATH . INCLUDES . 'component-footer.php';
@@ -18,8 +19,11 @@ require_once ABSPATH . INCLUDES . 'layout-default.php';
 require_once ABSPATH . INCLUDES . 'layout-collection-list.php';
 require_once ABSPATH . INCLUDES . 'layout-collection-single.php';
 require_once ABSPATH . INCLUDES . 'layout-announcements.php';
+
 User::create_table();
 Gradable::create_table();
+Grade::create_table();
+
 User::insert((object) [
   'username' => 'chehabom',
   'password' => 'chehabom',
@@ -33,9 +37,7 @@ User::insert((object) [
 User::updateByUsername('chehabom', 's', [
   'role' => 'ta',
 ]);
-
 debug(User::selectByUsername('gebotysb'));
 debug(User::selectAllByRole('ta'));
-
 User::deleteByUsername('gebotysb');
 
