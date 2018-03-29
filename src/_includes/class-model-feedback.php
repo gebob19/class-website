3c509da244625b85bfeb720c153585319cba34f1
Brennan Gebotys <gebob19.git@gmail.com>
2018-03-29 17:23:56 -0400

    fixed typos

--- src/_includes/class-model-feedback.php
+++ src/_includes/class-model-feedback.php
@@ -1,7 +1,7 @@
 <?php
 Class Feedback implements Model {
   public static function create_table() {
-    query("CREATE TABLE IF NOT EXISTS `Feature`(
+    query("CREATE TABLE IF NOT EXISTS `Feedback`(
       `id` bigint auto_increment,
       `instructor_username` varchar(255) unique NOT NULL,
       `message` varchar(100) NOT NULL,
@@ -14,24 +14,28 @@ Class Feedback implements Model {
 
   public static function selectAll() {
     return query("SELECT `id`, 
+      `instructor_username`,
       `message`,
       `created_at`, 
       `updated_at`, 
-      FROM `Feature`");
+      FROM `Feedback`");
   }
 
   public static function insert($data) {
     $now = now();
-    return query("INSERT INTO `Feature` (
+    return query("INSERT INTO `Feedback` (
+      `instructor_username`,
       `message`,
       `created_at`, 
       `updated_at`, 
     ) VALUES (
       ?,
       ?,
+      ?,
       ?
     )", [
       'sss',
+      $data->instructor_username,
       $data->message,
       $now,
       $now
