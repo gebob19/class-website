9954456d7c7d9e1ad411e347ca074f9199bff8aa
Brennan Gebotys <gebob19.git@gmail.com>
2018-03-28 23:26:20 -0400

    Feedback impl

---
+++ src/_includes/class-model-feedback.php
@@ -0,0 +1,40 @@
+<?php
+Class Feedback implements Model {
+  public static function create_table() {
+    query("CREATE TABLE IF NOT EXISTS `Feature`(
+      `id` bigint auto_increment,
+      `message` varchar(100) NOT NULL,
+      `created_at` datetime NOT NULL,
+      `updated_at` datetime NOT NULL,
+      PRIMARY KEY (`id`),
+      FOREIGN KEY (`instructor_username`) REFERENCES User(`username`)
+      )");
+  }
+
+  public static function selectAll() {
+    return query("SELECT `id`, 
+      `message`,
+      `created_at`, 
+      `updated_at`, 
+      FROM `Feature`");
+  }
+
+  public static function insert($data) {
+    $now = now();
+    return query("INSERT INTO `Feature` (
+      `message`,
+      `created_at`, 
+      `updated_at`, 
+    ) VALUES (
+      ?,
+      ?,
+      ?
+    )", [
+      'sss',
+      $data->message,
+      $now,
+      $now
+    ]);
+  }
+}
+?>
