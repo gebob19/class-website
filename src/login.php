a12934b99a47d026f3867caee45ccc94a975c4e6
Omar Chehab <omarchehab98@gmail.com>
2018-04-03 05:24:08 -0400

    Update login page styling

--- src/login.php
+++ src/login.php
@@ -2,12 +2,38 @@
   require_once 'config.php';
 
   if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+    $page = (object) [
+      'title' => 'Login',
+    ];
     function component_content() {
       ?>
       <form action="login.php" method="POST">
-        <input name='username'/>
-        <input name='password' type='password'/>
-        <input type='submit'/>
+        <label>
+          Username
+          <br />
+          <input name="username"
+            type="text"
+            placeholder="Username"
+            minlength="3"
+            maxlength="32"
+            pattern="^\w+\d*$"
+            title="One or more characters followed by zero or more digits."
+            required />
+        </label>
+        
+        <label>
+          Password
+          <br />
+          <input name="password"
+            type="password"
+            placeholder="Password"
+            minlength="8"
+            maxlength="64"
+            required />
+        </label>
+
+        <input type="submit"
+          value="Login" />
       </form>
       <?php
     }
