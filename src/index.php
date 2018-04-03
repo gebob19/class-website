ae5f1584bcd7dd4feefc79f7b4205ad0a69bd572
Omar Chehab <omarchehab98@gmail.com>
2018-04-03 06:16:32 -0400

    Hide nav when logged out

--- src/index.php
+++ src/index.php
@@ -1,11 +1,19 @@
 <?php
 require_once 'config.php';
 
-function component_content() {
-  $announcements = Announcement::select();
-  foreach ($announcements as $announcement) {
-    component_announcement($announcement);
-  }
-}
+switch ($_SERVER['REQUEST_METHOD']) {
+  case 'GET':
+    $page = (object) [
+      'title' => 'Announcements',
+    ];
 
-layout_default();
+    function component_content() {
+      $announcements = Announcement::select();
+      foreach ($announcements as $announcement) {
+        component_announcement($announcement);
+      }
+    }
+
+    layout_default();
+    exit();
+}
