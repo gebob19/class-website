6af293d7b04e50366a8840768098a40ba0e4620d
Brennan Gebotys <gebob19.git@gmail.com>
2018-04-03 12:48:31 -0400

    seperated feedback and grades permission

--- src/index.php
+++ src/index.php
@@ -13,12 +13,14 @@ switch ($_SERVER['REQUEST_METHOD']) {
           Welcome <?= $_SESSION['username'] ?>, <a href="/student-grades.php">click here</a> to see your current
           grades.
         </p>
+      <?php endif;
+
+      if (is_logged_in() && has_page_permission($_SESSION['role'], '/feedback.php')): ?>
         <p>
           <a href='/feedback.php'>Submit anonymous feedback here</a>
         </p>
       <?php endif;
 
-      
       $announcements = Announcement::select();
       foreach ($announcements as $announcement) {
         component_announcement($announcement);
