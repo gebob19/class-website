8d41b12c4a53a86e9bb5fcd1922624a50ead8719
Omar Chehab <omarchehab98@gmail.com>
2018-04-04 18:46:14 -0400

    Disallow ta to see grades

--- src/class-grades.php
+++ src/class-grades.php
@@ -11,68 +11,72 @@ switch ($_SERVER['REQUEST_METHOD']) {
       $gradables = Gradable::select();
       $students = User::selectByRole('student');
       foreach ($gradables as $gradable):
-        $grades = Grade::selectByGradableName($gradable->name); ?>
+        $grades = $_SESSION['role'] == 'instructor'
+          ? Grade::selectByGradableName($gradable->name)
+          : []; ?>
         <h4><?= $gradable->name ?></h4>
 
-        <div class="studentgrades--table">
-          <div class="studentgrades--table-row studentgrades--table-firstrow">
-            <span class="studentgrades--table-row-cell">
-              Student
-            </span>
-            <span class="studentgrades--table-row-cell">
-              Grade
-            </span>
-          </div>
-
-          <?php foreach ($grades as $grade): ?>
-            <div class="studentgrades--table-row">
+        <?php if (count($grades) > 0): ?>
+          <div class="studentgrades--table">
+            <div class="studentgrades--table-row studentgrades--table-firstrow">
               <span class="studentgrades--table-row-cell">
-                <?= $grade->student_username ?>
+                Student
               </span>
               <span class="studentgrades--table-row-cell">
-                <?= $grade->grade?>
+                Grade
               </span>
             </div>
+
+            <?php foreach ($grades as $grade): ?>
+              <div class="studentgrades--table-row">
+                <span class="studentgrades--table-row-cell">
+                  <?= $grade->student_username ?>
+                </span>
+                <span class="studentgrades--table-row-cell">
+                  <?= $grade->grade?>
+                </span>
+              </div>
             <?php endforeach; ?>
           </div>
+        <?php endif; ?>
 
-          <form class="classgrades--updategrade"
-            action="/update-grade.php"
-            method="POST"
-            style="padding-top: 1em;">
-            <div class="classgrades--updategrade-col">
-              <label>
-                <span>Student</span>
-                <select name="student_username" required>
-                  <option value=""></option>
-                  <?php foreach ($students as $student): ?>
-                    <option value="<?= $student->username ?>">
-                      <?= $student->username ?>
-                    </option>
-                  <?php endforeach; ?>
-                </select>
-              </label>
-            </div>
-            <div class="classgrades--updategrade-col">
-              <label>
-                <span>Grade</span>
-                <input name="grade"
-                  type="number"
-                  min="0"
-                  max="1"
-                  step="0.01"
-                  required />
-              </label>
-            </div>
-            <div class="classgrades--updategrade-col">
-              <input type="text"
-                name="gradable_name"
-                value="<?=$gradable->name?>"
-                style="display: none">
-              <input type="submit" value="Update"/>
-            </div>
-          </form>
-        <?php endforeach;
+        <form class="classgrades--updategrade"
+          action="/update-grade.php"
+          method="POST"
+          style="padding-top: 1em;">
+          <div class="classgrades--updategrade-col">
+            <label>
+              <span>Student</span>
+              <select name="student_username" required>
+                <option value=""></option>
+                <?php foreach ($students as $student): ?>
+                  <option value="<?= $student->username ?>">
+                    <?= $student->username ?>
+                  </option>
+                <?php endforeach; ?>
+              </select>
+            </label>
+          </div>
+          <div class="classgrades--updategrade-col">
+            <label>
+              <span>Grade</span>
+              <input name="grade"
+                type="number"
+                min="0"
+                max="1"
+                step="0.01"
+                required />
+            </label>
+          </div>
+          <div class="classgrades--updategrade-col">
+            <input type="text"
+              name="gradable_name"
+              value="<?=$gradable->name?>"
+              style="display: none">
+            <input type="submit" value="Update"/>
+          </div>
+        </form>
+      <?php endforeach;
     }
     
     layout_default();
--- src/index.php
+++ src/index.php
@@ -24,7 +24,12 @@ switch ($_SERVER['REQUEST_METHOD']) {
       if (is_logged_in() && has_page_permission($_SESSION['role'], '/class-grades.php')): ?>
         <p>
           <a href='/class-grades.php'>
-          Welcome <?=$_SESSION['role']?> <?=$_SESSION['username']?>, click here to see all grades of your class</a>
+          <?php if ($_SESSION['role'] == 'instructor'): ?>
+            Welcome instructor <?=$_SESSION['username']?>, click here to see all grades of your class</a>
+          <?php endif; ?>
+          <?php if ($_SESSION['role'] == 'ta'): ?>
+            Welcome T.A. <?=$_SESSION['username']?>, click here to change grades of your class</a>
+          <?php endif; ?>
         </p>
       <?php endif;
 
